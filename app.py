ede11b49b13aa79a2985f761fbf0f2bb131a6215
Victor Mikhaylov <victor.v.mikhaylov@gmail.com>
2021-03-29 22:34:55 +0300

    CORS

--- app.py
+++ app.py
@@ -3,14 +3,9 @@
 import aws_cdk.aws_dynamodb as dynamodb
 from aws_cdk.core import App, Environment, Stack, Construct
 from aws_cdk.aws_dynamodb import Table, Attribute, AttributeType, BillingMode
-from aws_cdk.aws_s3 import Bucket
+from aws_cdk.aws_s3 import Bucket, HttpMethods
 from aws_cdk.aws_lambda import Function, Code, Runtime
-from aws_cdk.aws_apigateway import (
-    RestApi,
-    MockIntegration,
-    PassthroughBehavior,
-    LambdaIntegration,
-)
+from aws_cdk.aws_apigateway import RestApi, LambdaIntegration, Cors
 
 STORAGE_ID = "Votes"
 
@@ -25,7 +20,8 @@ class VotingStorageStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        Table(self,
+        Table(
+            self,
             id=STORAGE_ID,
             table_name=STORAGE_ID,
             partition_key=Attribute(name="voter", type=AttributeType.STRING),
@@ -37,7 +33,8 @@ class VotingVoteStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        bucket = Bucket(self,
+        bucket = Bucket(
+            self,
             id="voting-frontend",
             bucket_name="voting-frontend-bkt",
             public_read_access=True,
@@ -49,14 +46,16 @@ class VotingResultStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        bucket = Bucket(self,
+        bucket = Bucket(
+            self,
             id="result-frontend",
             bucket_name="result-frontend-bkt",
             public_read_access=True,
             website_index_document="index.html",
         )
 
-        handler = Function(self,
+        handler = Function(
+            self,
             id="result-backend",
             function_name="result-backend-function",
             code=Code.asset("./result-backend"),
@@ -64,14 +63,46 @@ class VotingResultStack(Stack):
             runtime=Runtime.PYTHON_3_8,
         )
 
-        dbtable = dynamodb.Table.from_table_name(self, STORAGE_ID, table_name=STORAGE_ID)
+        dbtable = dynamodb.Table.from_table_name(
+            self, STORAGE_ID, table_name=STORAGE_ID
+        )
         dbtable.grant_read_data(handler)
 
         rest_gw = RestApi(
-            self, id="result-gateway", rest_api_name="result-gateway-api",
+            self,
+            id="result-gateway",
+            rest_api_name="result-gateway-api",
+            default_cors_preflight_options={
+                "allow_origins": Cors.ALL_ORIGINS,
+                "allow_methods": Cors.ALL_METHODS,
+            },
         )
         gw_resource = rest_gw.root.add_resource("my-vote")
-        gw_resource.add_method("GET", LambdaIntegration(handler))
+        gw_resource_lambda_integration = LambdaIntegration(
+            handler,
+            proxy=True,
+            integration_responses=[
+                {
+                    "statusCode": "200",
+                    "responseParameters": {
+                        "method.response.header.Access-Control-Allow-Origin": "'*'",
+                    },
+                }
+            ],
+        )
+        gw_resource.add_method(
+            "GET",
+            gw_resource_lambda_integration,
+            method_responses=[
+                {
+                    "statusCode": "200",
+                    "responseParameters": {
+                        "method.response.header.Access-Control-Allow-Origin": True,
+                    },
+                }
+            ],
+        )
+
 
 VotingStorageStack(app, "voting-app-storage-stack", env=env, tags=tags)
 VotingResultStack(app, "voting-app-result-stack", env=env, tags=tags)
